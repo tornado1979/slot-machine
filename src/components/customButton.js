ced18150f6c9b96fc72e960b58b5b994d82f7151
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-30 11:47:40 +0300

    Add disabled property on the 'spin' button

--- src/components/customButton.js
+++ src/components/customButton.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
-export const CustomButton = ({ handleClick }) => {
+export const CustomButton = ({ disable, handleClick }) => {
   return (
-    <button onClick={handleClick} type="button">
+    <button className="spin" disabled={disable} onClick={handleClick} type="button">
       Spin
     </button>
   )
 }
 
 CustomButton.propTypes = {
+  disable: propTypes.bool.isRequired,
   handleClick: propTypes.func.isRequired,
 }
