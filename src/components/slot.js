16ad6e76a8daebf503ce11fc9765984d5557f8ac
Theodoros Itzaris <theodoreme@gmail.com>
2018-07-30 11:49:12 +0300

    Slot gets a default image when it loads

--- src/components/slot.js
+++ src/components/slot.js
@@ -5,6 +5,7 @@ import apple from '../assets/img/apple.png'
 import lemon from '../assets/img/lemon.jpeg'
 import cherry from '../assets/img/cherry.jpeg'
 import banana from '../assets/img/banana.jpeg'
+import init from '../assets/img/default.jpeg'
 
 export const Slot = ({ data, slot }) => {
   const imgObject = {
@@ -16,13 +17,23 @@ export const Slot = ({ data, slot }) => {
   const src = `http://localhost:3000/src/assets/img/${data[slot]}`
   return (
     <div>
-      {data[slot]}
-      <img alt={data[slot]} src={imgObject[data[slot]]} />
+      {slot === init && <img alt="default" src={init} />}
+      {slot !== init && <img alt={data[slot]} src={imgObject[data[slot]]} />}
+      <div>
+        {data[slot]}
+      </div>
     </div>
   )
 }
 
+Slot.defaultProps = {
+  slot: init,
+}
+
 Slot.propTypes = {
   data: propTypes.array.isRequired, // eslint-disable-line
-  slot: propTypes.number.isRequired,
+  slot: propTypes.oneOfType([
+    propTypes.number,
+    propTypes.string,
+  ]),
 }
